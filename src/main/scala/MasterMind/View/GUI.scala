d78dfbd2182a5789841ff8a4ef64829b0ba7943b
Luca Salvigni <luca.salvigni7@studio.unibo.it>
2021-03-16 14:50:35 +0100

    Change GUI's layout

--- src/main/scala/MasterMind/View/GUI.scala
+++ src/main/scala/MasterMind/View/GUI.scala
@@ -3,18 +3,20 @@ package MasterMind.View
 import java.awt.Color
 
 import MasterMind.Model.GameController
-import MasterMind.Utility.InitializeControllerMsg
+import MasterMind.Utility.{InitializeControllerMsg, Msg}
 import akka.actor.typed.ActorSystem
 import javax.swing.border.LineBorder
 
+import scala.collection.mutable.ListBuffer
 import scala.util.control.Exception.allCatch
 import scala.swing.event.ButtonClicked
-import scala.swing.{BoxPanel, Button, CheckBox, Dialog, Dimension, FlowPanel, Font, GridPanel, Label, MainFrame, Orientation, ScrollPane, TextArea, TextField}
+import scala.swing.{BoxPanel, Button, CheckBox, Dialog, Dimension, FlowPanel, Font, Label, MainFrame, Orientation, RadioButton, ScrollPane, TextArea, TextField}
 
 object GUI extends MainFrame {
 
-  val gameSystem = ActorSystem(GameController(), "GameSystem")
-  val gameBoard = Game()
+  val gameSystem: ActorSystem[Msg] = ActorSystem(GameController(), "GameSystem")
+  var gameBoard: GameBoard = _
+  var humanPanel: HumanPanel = _
 
   def top: MainFrame = new MainFrame() {
     startGame().open()
@@ -27,7 +29,6 @@ object GUI extends MainFrame {
   def logChat(msg: String) : Unit = {
     gameBoard.logChat.text += "\n> " + msg
   }
-
 }
 
 /**
@@ -40,14 +41,21 @@ class startingGameDialog extends Dialog {
   val startButton: Button = new Button("START") {
     reactions += {
       case ButtonClicked(_) =>
-      isNumber(numPlayers.text) match {
-        case true => isNumber(textCodeLength.text) match {
-          case true => close()
-            GUI.gameSystem ! InitializeControllerMsg(numPlayers.text.toInt,textCodeLength.text.toInt,withHuman = false,sharedResponses = false)
-            GUI.gameBoard.open()
-          case _ => Dialog.showMessage(contents.head, "Select a valid code's length", "ERROR!", Dialog.Message.Info, null)
+      if (isNumber(numPlayers.text)) {
+        if (isNumber(textCodeLength.text)) {
+          close()
+          var isPresent: Boolean = false
+          if (humanCheck.selected) {
+            isPresent = true
+          }
+          GUI.gameBoard = Game(isPresent, numPlayers.text.toInt)
+          GUI.gameSystem ! InitializeControllerMsg(numPlayers.text.toInt, textCodeLength.text.toInt, isPresent, sharedResponses = false)
+          GUI.gameBoard.open()
+        } else {
+          Dialog.showMessage(contents.head, "Select a valid code's length", "ERROR!", Dialog.Message.Info, null)
         }
-        case _ => Dialog.showMessage(contents.head, "Select a valid number of player", "ERROR!", Dialog.Message.Info, null)
+      } else {
+        Dialog.showMessage(contents.head, "Select a valid number of player", "ERROR!", Dialog.Message.Info, null)
       }
     }
   }
@@ -90,15 +98,30 @@ object startGame {
 /**
  * Representing the Mastermind's board
  */
-class GameBoard() extends Dialog {
+class GameBoard(withHuman: Boolean, nPlayers: Int) extends Dialog {
   val logChat: TextArea = new TextArea("GAME IS STARTED!")
   logChat.editable = false
   logChat.border = new LineBorder(Color.BLACK, 2)
   logChat.font = Font("monospaced", Font.Italic, 15)
 
-  title = "Mastermind's GameBoard"
-  contents = new ScrollPane(logChat)
+  var startStop: Button = new Button("Stop") {
+    reactions += {
+      case ButtonClicked(_) => startStop.text match {
+        case "Stop" => startStop.text ="Start"; println("Stop the game")
+        case _ => startStop.text ="Stop"; println("Resume the game")
+      }
+    }
+  }
+  startStop.preferredSize = new Dimension(250,40)
+  startStop.xLayoutAlignment = 0.5
 
+  contents = new BoxPanel(Orientation.Vertical) {
+    if(withHuman) GUI.humanPanel = HumanPanelObj(nPlayers); contents += GUI.humanPanel
+    contents += new ScrollPane(logChat)
+    contents += startStop
+  }
+
+  title = "Mastermind's GameBoard"
   size = new Dimension(700, 500)
   this.peer.setLocationRelativeTo(null)
 
@@ -108,8 +131,36 @@ class GameBoard() extends Dialog {
   }
 }
 
-object Game extends {
-  def apply(): GameBoard = new GameBoard()
+object Game {
+  def apply(withHuman: Boolean, nPlayers: Int): GameBoard = new GameBoard(withHuman, nPlayers)
 }
 
+class HumanPanel(nPlayers: Int) extends BoxPanel(Orientation.Vertical) {
+  var radioPlayer = new ListBuffer[RadioButton]()
+  val selectNumber: TextField = new TextField() {
+    maximumSize = new Dimension(150,40)
+  }
+  val sendGuess = new Button("Send")
 
+  contents += new BoxPanel(Orientation.Horizontal) {
+    contents += new Label("Select a player: ")
+    println(nPlayers)
+    for (i <- 0 until nPlayers) {
+      radioPlayer += new RadioButton(" Player" + i) {
+        reactions += {
+          case ButtonClicked(_) => for(a <- radioPlayer) a.selected = false; selected = true
+        }
+      }
+    }
+    contents ++= radioPlayer
+  }
+  contents += new BoxPanel(Orientation.Horizontal) {
+    contents += new Label("Select a number:  ")
+    contents += selectNumber
+    contents += sendGuess
+  }
+}
+
+object HumanPanelObj {
+  def apply(nPlayers: Int): HumanPanel = new HumanPanel(nPlayers)
+}
